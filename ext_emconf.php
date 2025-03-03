abdbb103f7685e0678d5ab14a5e6d187dd912a4f
Rico Sonntag <rico.sonntag@netresearch.de>
2025-03-03 11:31:55 +0000

    WEB-1237: Create base extension structure

---
+++ ext_emconf.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * This file is part of the package meine-krankenkasse/typo3-search-algolia.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+$EM_CONF['typo3_search_algolia'] = [
+    'title'          => 'Meine Krankenkasse: TYPO3 Search Algolia',
+    'description'    => 'A TYPO3 extension that integrates Algolia search into your website by indexing TYPO3 content for lightning-fast, relevant search results.',
+    'category'       => 'module',
+    'author'         => 'Rico Sonntag',
+    'author_email'   => 'rico.sonntag@netresearch.de',
+    'author_company' => 'Netresearch DTT GmbH',
+    'state'          => 'stable',
+    'version'        => '1.0.0',
+    'constraints'    => [
+        'depends' => [
+            'typo3' => '12.4.0-12.99.99',
+        ],
+        'conflicts' => [
+        ],
+        'suggests' => [
+        ],
+    ],
+];
---
+++ ext_localconf.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * This file is part of the package meine-krankenkasse/typo3-search-algolia.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+defined('TYPO3') || exit('Access denied.');
+
+call_user_func(static function (): void {
+});
